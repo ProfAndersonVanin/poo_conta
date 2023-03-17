9f6e6aa54c85e3235b5f6ba7efa79c31d3ed0473
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2023-03-17 12:35:15 -0300

    02-Metodos

---
+++ projeto_conta_02-Metodos/Conta.cs
@@ -0,0 +1,30 @@
+﻿namespace projeto_conta;
+class Conta
+{
+    public double Saldo {get;set;}    
+    public double Limite {get;set;}
+    public int Numero {get;set;}
+
+    //get significa que vai acessar os valores deste atributo
+    //set significa que vai setar algum valor neste atributo
+
+    //Métodos são as ações dentro da classe
+
+    //Método Deposita - recebe um valor e acrescenta 
+    public void Deposita(double valor){
+        //agora vamos pegar o valor que está vindo como um parâmetro do método e atribuí-lo ao atributo Saldo
+        //o this, consegue acessar todos os atributos da classe e manipulá-los
+       this.Saldo += valor; //incrementa o saldo em + valor
+    }
+
+    //Método ConsultaSaldo - retorna um saldo disponível
+    public double ConsultaSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    
+
+
+
+
+}
---
+++ projeto_conta_02-Metodos/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace projeto_conta;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Conta Bancária");
+
+        //Aqui vamos instanciar um objeto chamado conta1 a partir da Classe Conta
+        Conta conta = new Conta();
+
+        //Vamos atribuir um valor de limite para essa conta
+        conta.Limite = 500;
+
+        //Para começar vamos chamar o método Deposita para depositar um valor na conta e o saldo dela já ser atualizado
+        conta.Deposita(1500); 
+        conta.Deposita(2500);
+        conta.Deposita(4500);
+
+        //Depois que uma classe possui um método que seta e consulta valores dos atributos, não é mais necessário fazer a atribuição de um determinado atributo diretamente como foi feito na aula anterior. Ex:
+        //conta.Saldo = 1500; ==> isso não é mais necessário
+        //utilizamos os métodos para fazer as validações necessárias
+
+        //veja o que acontece:
+        conta.Saldo = 10; //ERRADO!!!      
+
+        //Vamos chamar o método ConsultaSaldo e receber em uma variável local chamada saldo o valor disponível em saldo desse cliente
+        double saldo = conta.ConsultaSaldo();
+        //Agora para visualizar:
+        Console.WriteLine("Seu saldo é de: " + saldo);
+
+        
+
+        
+    }
+}
