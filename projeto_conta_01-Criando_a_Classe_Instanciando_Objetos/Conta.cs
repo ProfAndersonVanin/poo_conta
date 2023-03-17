1f539a8d9e9e9a6b9bb201e6405d0d3dd96e3589
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2023-03-17 12:34:34 -0300

    01-Criando_a_Classe_Instanciando_Objetos

---
+++ projeto_conta_01-Criando_a_Classe_Instanciando_Objetos/Conta.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_conta;
+class Conta
+{
+    public double Saldo {get;set;}    
+    public double Limite {get;set;}
+    public int Numero {get;set;}
+
+    //get significa que vai acessar os valores deste atributo
+    //set significa que vai setar algum valor neste atributo
+
+
+
+}
---
+++ projeto_conta_01-Criando_a_Classe_Instanciando_Objetos/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace projeto_conta;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Conta Bancária");
+
+        //Aqui vamos instanciar um objeto chamado conta1 a partir da Classe Conta
+        Conta conta1 = new Conta();
+
+        //Aqui vamos através do objeto conta1, acessar os atributos da classe Conta e setar alguns valores iniciais para os atributos (usamos o set)
+        conta1.Saldo = 1500;
+        conta1.Limite = 500;
+        conta1.Numero = 123;
+
+        //Aqui buscamos a informação que está armazenada nos atributos (usamos o get)
+        Console.WriteLine("Olá, fulano, seu saldo é de: " + conta1.Saldo);
+        Console.WriteLine("Seu Limite de saque é de: " + conta1.Limite);
+        Console.WriteLine("O número de sua conta é: " + conta1.Numero);
+
+        //Agora faça você: Instancie um outro objeto chamado conta2, atribua um saldo, limite e número da conta. Mostre todas as informações da conta2 também.
+
+    }
+}
