3462904860845367b8e5813d27a72b7140e0f565
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2023-03-17 12:36:44 -0300

    03-Metodos_Proteger_Atributos_Parte02

---
+++ projeto_conta_03-Metodos_Proteger_Atributos_Parte02/Conta.cs
@@ -0,0 +1,26 @@
+﻿namespace projeto_conta;
+class Conta
+{
+    private double Saldo {get; set;}  //Tudo private
+    public double Limite {get;private set;} //só set private
+    public int Numero {get;set;}
+    
+    public void Deposita(double valor){
+        this.Saldo += valor; 
+    }
+    
+    public double ConsultaSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    //Adicionando método para atualizar um limite
+    public void AdicionarLimite(double valor){
+        this.Limite = valor; 
+    }
+
+    //Adicionando um método para Sacar
+    public void Sacar(double valor){
+        this.Saldo -= valor; 
+        //Aqui ainda temos um problema que é o caso de sacar um valor que seja maior do que o disponível!!!
+    }
+}
---
+++ projeto_conta_03-Metodos_Proteger_Atributos_Parte02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace projeto_conta;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Conta Bancária");Conta conta = new Conta();
+
+        //conta.Limite = 500; //Aqui também podemos criar um método na classe que adiciona este limite
+
+        conta.AdicionarLimite(500);
+
+        conta.Deposita(1500); 
+        conta.Deposita(2500);
+        conta.Deposita(4500);
+
+        conta.Sacar(7000);
+          
+        double saldo = conta.ConsultaSaldo();
+        
+        Console.WriteLine("Seu saldo com limite é de: " + saldo);
+        //Console.WriteLine("Seu saldo é de: " + conta.Saldo); //Aqui não temos mais acesso direto ao atributo Saldo
+
+        Console.WriteLine("Seu limite é de: " + conta.Limite);
+
+                        
+    }
+}
