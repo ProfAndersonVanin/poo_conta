2201d7d8d9f5997064275203f2b604bd5222a789
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2023-03-17 12:35:59 -0300

    03-Metodos_Proteger_Atributos

---
+++ projeto_conta_03-Metodos_Proteger_Atributos/Conta.cs
@@ -0,0 +1,17 @@
+﻿namespace projeto_conta;
+class Conta
+{
+    public double Saldo {get; private set;}  //alterando o set para private - O private é um modificador de acesso
+    //Atributo Saldo é público, mas somente para get (consulta)
+    //Assim apenas a classe Conta consegue setar um valor
+    public double Limite {get;set;}
+    public int Numero {get;set;}
+    
+    public void Deposita(double valor){
+        this.Saldo += valor; 
+    }
+    
+    public double ConsultaSaldo(){
+        return this.Saldo + this.Limite;
+    }
+}
---
+++ projeto_conta_03-Metodos_Proteger_Atributos/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace projeto_conta;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Conta Bancária");Conta conta = new Conta();
+
+        conta.Limite = 500;
+
+        conta.Deposita(1500); 
+        conta.Deposita(2500);
+        conta.Deposita(4500);
+          
+        double saldo = conta.ConsultaSaldo();
+        
+        Console.WriteLine("Seu saldo é de: " + saldo); //Aqui como ele está usando o método traz o saldo + limite
+        Console.WriteLine("Seu saldo é de: " + conta.Saldo); //Aqui ele só pega o valor do saldo sem limite
+
+        //Aqui temos um problema:
+        //Projeto Conta Bancária
+        //Seu saldo é de: 9000
+        //Seu saldo é de: 8500
+
+        //SOLUÇÃO: DEIXAR O ATRIBUTO SALDO DA CLASSE TODO PRIVATE.
+
+
+                
+    }
+}
